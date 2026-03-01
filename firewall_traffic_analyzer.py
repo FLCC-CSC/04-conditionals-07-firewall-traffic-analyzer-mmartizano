54594eaa4d55d3f39e2a4cd98e953cd6f4aa4093
Michael Martizano <mmartizano@fingerlakes.edu>
2026-03-01 02:32:22 -0500

    Update firewall_traffic_analyzer.py

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -1,8 +1,8 @@
 # FILE NAME - firewall_traffic_analyzer.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Michael Martizano
+# DATE: 3/1/2026
+# BRIEF DESCRIPTION:  user inputs port number and transfer size
 
 
 
@@ -14,12 +14,35 @@
 
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
+def main():
+    firewall()
+
+def firewall():
+    print('=== Network Traffic Security Analyzer ===')
+    print()
+
+    port_num = int(input('Enter the port number (e.g., 80, 22, 443, 3389): '))
+    data_size = int(input('Enter the data transfer size in megabytes (MB): '))
+
+    print()
+    print('FIRWALL LOG')
+    print(f'Port: {port_num}, Transfer Size: {data_size} MB')
+
+    if (port_num == 22 or port_num == 3389) and data_size >= 100:
+        risk = 'HIGH RISK: Potential unauthorized remote access detected!'
+    elif port_num == 80 and data_size > 100:
+        risk = 'MEDIUM RISK: Large unencrypted data transfer detected.'
+    elif port_num == 443:
+        risk = 'LOW RISK: Secure encrypted transfer detected.'
+    else:
+        risk = 'UNKNOWN: Unrecognized traffice pattern'
+    
+    print(f'Risk Assessment: {risk}')
+    print('------------------------')
 
 
 
-
-
-
+main()
 
 
 
@@ -50,10 +73,10 @@ Risk Assessment: MEDIUM RISK: Large unencrypted data transfer detected.
 === Network Traffic Security Analyzer ===
 
 Enter the port number (e.g., 80, 22, 443, 3389): 22
-Enter the data transfer size in megabytes (MB): 12
+Enter the data transfer size in megabytes (MB): 1200
 
 FIREWALL LOG:
-Port: 22, Transfer Size: 12 MB
+Port: 22, Transfer Size: 1200 MB
 Risk Assessment: HIGH RISK: Potential unauthorized remote access detected!
 ------------------------
 '''
@@ -89,6 +112,7 @@ Risk Assessment: UNKNOWN: Unrecognized traffic pattern.
 '''
 
 1. Did you get tripped up using the `or` or `and` operators? If so, how?
+I did not since i'v used them in JAVA and C++
 
 
 
